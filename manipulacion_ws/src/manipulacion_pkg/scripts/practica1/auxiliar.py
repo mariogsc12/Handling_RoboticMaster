b733ebd3d2d62e55eb4d449a38f476aa0b2b4f43
Mario Gomez <m.gomez03016@gmail.com>
2026-02-06 23:22:04 +0000

    get best grasp and check colisions

--- manipulacion_ws/src/manipulacion_pkg/scripts/practica1/auxiliar.py
+++ manipulacion_ws/src/manipulacion_pkg/scripts/practica1/auxiliar.py
@@ -1,6 +1,10 @@
 from manipulacion_lib import SimulacionGripperFlotante
 from typing import Optional, List, Union
-
+from dataclasses import dataclass
+import yaml
+from pathlib import Path
+from typing import List
+ 
 GRIPPER_MODES = ["open", "close", "custom"]
 def set_gripper_pos(gripper: SimulacionGripperFlotante, mode: str = "open", articulaciones:Optional[List[Union[float, int]]]  = None):
     if not mode or mode not in GRIPPER_MODES:
@@ -23,3 +27,26 @@ def set_gripper_pos(gripper: SimulacionGripperFlotante, mode: str = "open", arti
             raise ValueError("El valor de las articulaciones no ha sido definido")
         
     gripper.set_posicion_articulaciones(posicion_articulaciones)
+
+@dataclass
+class GraspInfo:
+    dofs: list
+    pose: list
+    epsilon_quality: float
+    volume_quality: float
+
+
+def get_grasps_list(grasp_poses_path: Path):
+    with open(grasp_poses_path, "r") as f:
+        data = yaml.safe_load(f)
+
+    grasp_list: List[GraspInfo] = []
+
+    for grasp in data["grasps"]:
+        grasp = GraspInfo(dofs=grasp["dofs"],
+                         pose=grasp["pose"],
+                         epsilon_quality=grasp['epsilon_quality'],
+                         volume_quality=grasp['volume_quality'])
+        grasp_list.append(grasp)
+
+    return grasp_list
---
+++ manipulacion_ws/src/manipulacion_pkg/scripts/practica1/configuration.py
@@ -0,0 +1,5 @@
+# Definir variables globales con el nombre del gripper y objeto
+NOMBRE_GRIPPER_GAZEBO = "gripper"     
+NOMBRE_OBSTACULO_GAZEBO = "red_obstacle"
+NOMBRE_OBJETO = "driller_small"
+NOMBRE_GRIPPER = "robotiq"
